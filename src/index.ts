adf070b687f5460a1f5d330d9f1106b66958b137
julz <owanojulius742002@gmail.com>
2025-02-17 11:54:34 +0800

    Put url in cors origin

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,13 @@ config();
 
 const app = express();
 const httpServer = http.createServer(app)
-export const io = new Server(httpServer, { cors: { origin: "*" } });
+export const io = new Server(httpServer, { cors: { origin: '*' } });
 io.on('connection', socketIo);
 const PORT = process.env.PORT || 9091
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(cors({ credentials: true, origin: ['http://localhost:5173', 'http://localhost:4173', 'https://current-mala-noneee-8ebdc2b6.koyeb.app'] }));
 app.use(cookieParser());
 
 app.use("/api/uploadthing", createRouteHandler({ router: uploadRouter, config: {} }));
